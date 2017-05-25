f99a6b22a4609ab48e79146717388860734adba8
Ulrich Hung <ulle@ullebot.net>
2017-05-25 06:36:18 +0200

    test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // only for development/testing:
 // cd nvsx-helo
-// run this with "node test-runner"
+// run this with "node test"
 
 const nvsxHelo = require(__dirname + "/index.js");
 nvsxHelo.print();
